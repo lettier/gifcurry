1fb14216f0fa8c52a1399e2de2b034a58cf64a17
Lettier <contact@lettier.com>
2016-05-01 03:04:23 -0400

    Refactors input params. Adds type signatures. Cleans up syntax. Creates Gifcurry library. Adds documentation. Switches to forkOS. Allows for more flexible cli options.

--- Gifcurry.hs
+++ Gifcurry.hs
@@ -1,6 +1,13 @@
 -- David Lettier (C) 2016. http://www.lettier.com/
 
-module Gifcurry (gif) where
+-- | Produces GIFs using FFmpeg and ImageMagick.
+-- The main function is 'gif'.
+module Gifcurry (
+      gif
+    , GifParams(..)
+    , defaultGifParams
+    , gifParamsValid
+  ) where
 
 import System.Environment
 import System.Process
@@ -8,109 +15,230 @@ import System.IO.Temp
 import System.Directory
 import System.Exit
 import Data.List
+import Text.Printf
 import Control.Exception
+import Control.Monad
 
-gif [is, os, st, dr, wd, qa, ttx, btx] =
-  withTempDirectory "." "frames" $ \tmpDir -> do
-    putStrLn $ "\nInput file: " ++ is
-    putStrLn $ "Start second: " ++ st
-    putStrLn $ "Duration: " ++ dr ++ " seconds"
-    putStrLn $ "GIF width: " ++ wd ++ "px"
-    putStrLn $ "Quality: " ++ (quality qa) ++ "%"
-    putStrLn $ "Top text: " ++ ttx
-    putStrLn $ "Bottom text: " ++ btx
-    putStrLn $ "\nWriting temporary frames to... " ++ tmpDir
-    fileExists <- doesFileExist is
-    if fileExists
+-- | The data type record required by 'gif'.
+data GifParams = GifParams {
+      inputFile :: [Char]
+    , outputFile :: [Char]
+    , startTime :: Float
+    , durationTime :: Float
+    , widthSize :: Int
+    , qualityPercent :: Float
+    , topText :: [Char]
+    , bottomText :: [Char]
+  } deriving (Show, Read)
+
+-- | Specifies default parameters for 'startTime', 'durationTime', 'widthSize', and 'qualityPercent'.
+defaultGifParams = GifParams {
+      inputFile = ""
+    , outputFile = ""
+    , startTime = 0.0
+    , durationTime = 1.0
+    , widthSize = 500
+    , qualityPercent = 100.0
+    , topText = ""
+    , bottomText = ""
+  }
+
+-- | Inputs 'GifParams' and outputs either an IO IOError or IO String.
+--
+-- @
+--    import qualified Gifcurry (gif, GifParams(..), defaultGifParams, gifParamsValid)
+--    main :: IO ()
+--    main = do
+--      let params = Gifcurry.defaultGifParams { Gifcurry.inputFile = ".\/in.mov", Gifcurry.outputFile = ".\/out.gif" }
+--      valid <- Gifcurry.gifParamsValid params
+--      if valid
+--        then do
+--          result <- Gifcurry.gif params
+--          print result
+--        else return ()
+-- @
+gif :: GifParams -> IO (Either IOError String)
+gif gifParams =
+  withTempDirectory "." "frames" $ \tmpdir -> do
+    printGifParams gifParams tmpdir
+    validParams <- gifParamsValid gifParams
+    if validParams
       then do
-        result <- try (readProcess "ffmpeg" [
-          "-nostats",
-          "-loglevel",
-          "panic",
-          "-an",
-          "-ss",
-          st,
-          "-i",
-          is,
-          "-t",
-          dr,
-          "-r",
-          "15",
-          "-q:v",
-          "2",
-          "-vf",
-          "scale=" ++ wd ++ ":-1",
-          "-f",
-          "image2",
-          tmpDir ++ "/%010d.png" ] "") :: IO (Either IOError String)
-        result <- case result of
-          Left ex -> return 1
-          Right val -> return 0
-        putStrLn $ "Writing your GIF to... " ++ os
-        result <- try (convert qa tmpDir os wd ttx btx) :: IO (Either IOError String)
-        result <- case result of
-          Left ex -> return 1
-          Right val -> return 0
-        putStrLn "Done."
-        return result
-      else do
-        putStrLn "Video file does not exist."
-        return 0
+        result <- tryFfmpeg gifParams tmpdir
+        result' <- case result of
+          Left  err -> return False
+          Right val -> return True
+        if result'
+          then do
+            putStrLn $ "Writing your GIF to... " ++ outputFile gifParams
+            result <- tryConvert gifParams tmpdir
+            result' <- case result of
+               Left  err -> return False
+               Right val -> return True
+            if result'
+              then putStrLn "Done."
+              else putStrLn "[Error] Something when wrong with ImageMagick."
+            return result
+          else do
+            putStrLn "[Error] Something went wrong with FFmpeg."
+            return result
+      else return $ Left (userError "[Error] Invalid params.")
 
-convert qa dr os wd ttx btx = readProcess "convert" ([
-  "-quiet",
-  "-delay",
-  "6",
-  "-colors",
-  ncolors qa,
-  "-coalesce",
-  "-layers",
-  "OptimizeTransparency",
-  "-layers",
-  "RemoveDups",
-  dr ++ "/*.png",
-  "-dither",
-  "FloydSteinberg",
-  "-loop",
-  "0" ] ++ annotate wd ttx "north" ++ annotate wd btx "south" ++ [os]) ""
+-- | Outputs True or False if a GifParams record parameters are valid.
+-- Looks at 'inputFile', 'outputFile', 'startTime', 'durationTime', 'widthSize', and 'qualityPercent'.
+gifParamsValid :: GifParams -> IO Bool
+gifParamsValid GifParams {
+      inputFile = ipf
+    , outputFile = opf
+    , startTime = st
+    , durationTime = dt
+    , widthSize = ws
+    , qualityPercent = qp
+    , topText = tt
+    , bottomText = bt
+  } = do
+    inputFileExists <- case length ipf of
+      0 -> return False
+      _ -> doesFileExist ipf
+    unless inputFileExists $ putStrLn "\n[Error] Input video file does not exist."
+    let outputFileValid = length opf > 5
+    unless outputFileValid $ putStrLn "\n[Error] Output video file blank."
+    let valid = inputFileExists && outputFileValid && (st >= 0.0) && (dt >= 0.0) && (ws > 0) && (qp > 0.0)
+    unless valid $ putStrLn "\n[Error] Invalid params."
+    return valid
 
-quality qa
-  | n > 100   = show 100
-  | n < 0     = show 2
-  | otherwise = show n
-  where n = read qa :: Float
+printGifParams :: GifParams -> [Char] -> IO ()
+printGifParams
+  GifParams {
+      inputFile = ipf
+    , outputFile = opf
+    , startTime = st
+    , durationTime = dt
+    , widthSize = ws
+    , qualityPercent = qp
+    , topText = tt
+    , bottomText = bt
+  }
+  tmpdir = mapM_ putStrLn [
+        "\nInput file: " ++ ipf
+      , "Output file: " ++ opf
+      , "Start second: " ++ printf "%.3f" st
+      , "Duration: " ++ printf "%.3f" dt ++ " seconds"
+      , "GIF width: " ++ show ws ++ "px"
+      , "Quality: " ++ show (qualityPercentClamp qp) ++ "%"
+      , "Top text: " ++ tt
+      , "Bottom text: " ++ bt
+      , "\nWriting temporary frames to... " ++ tmpdir
+    ]
 
-ncolors qa
-  | n < 0.0    = show 1
-  | n >= 100.0 = show 256
-  | otherwise  = show $ truncate (n / 100.0 * 256.0)
-  where n = read (quality qa) :: Float
+tryFfmpeg :: GifParams -> [Char] -> IO (Either IOError String)
+tryFfmpeg
+  GifParams {
+      inputFile = ipf
+    , startTime = st
+    , durationTime = dt
+    , widthSize = ws
+  }
+  tmpdir = try(
+    readProcess "ffmpeg" [
+        "-nostats"
+      , "-loglevel"
+      , "panic"
+      , "-an"
+      , "-ss"
+      , sts
+      , "-i"
+      , ipf
+      , "-t"
+      , dts
+      , "-r"
+      , "15"
+      , "-q:v"
+      , "2"
+      , "-vf"
+      , "scale=" ++ wss ++ ":-1"
+      , "-f"
+      , "image2"
+      , tmpdir ++ "/%010d.png"
+    ] ""
+  ) :: IO (Either IOError String)
+  where sts = printf "%.3f" st
+        dts = printf "%.3f" dt
+        wss = show ws
 
-annotate wd tx dr = [
-  "-gravity",
-  dr,
-  "-stroke",
-  "#000C",
-  "-strokewidth",
-  "10",
-  "-pointsize",
-  pointsize wd tx,
-  "-annotate",
-  "+0+10",
-  tx,
-  "-stroke",
-  "none",
-  "-fill",
-  "white",
-  "-pointsize",
-  pointsize wd tx,
-  "-annotate",
-  "+0+10",
-  tx ]
+tryConvert :: GifParams -> [Char] -> IO (Either IOError String)
+tryConvert
+  GifParams {
+      outputFile = opf
+    , widthSize = ws
+    , qualityPercent = qp
+    , topText = tt
+    , bottomText = bt
+  }
+  tmpdir = try(
+    readProcess "convert" (
+      [
+          "-quiet"
+        , "-delay"
+        , "6"
+        , "-colors"
+        , show $ ncolors qp
+        , "-coalesce"
+        , "-layers"
+        , "OptimizeTransparency"
+        , "-layers"
+        , "RemoveDups"
+        , tmpdir ++ "/*.png"
+        , "-dither"
+        , "FloydSteinberg"
+        , "-loop"
+        , "0"
+      ] ++ annotate ws tt "north" ++ annotate ws bt "south" ++ [opf]
+    ) ""
+  )
 
-pointsize wd tx
-  | length tx == 0 = show 0
-  | n <= 0         = show 0
-  | otherwise      = show $ truncate ((n * 0.4) / l * (72.0 / 34.0))
-  where n = read wd :: Float
-        l = fromIntegral (length tx)
+qualityPercentClamp :: Float -> Float
+qualityPercentClamp qp
+  | qp > 100.0   = 100.0
+  | qp < 0.0     = 2.0
+  | otherwise    = qp
+
+ncolors :: Float -> Int
+ncolors qp
+  | qpc < 0.0    = 1
+  | qpc >= 100.0 = 256
+  | otherwise  = truncate (qpc / 100.0 * 256.0)
+  where qpc = qualityPercentClamp qp
+
+annotate :: Int -> [Char] -> [Char] -> [[Char]]
+annotate widthSize text topBottom = [
+      "-gravity"
+    , topBottom
+    , "-stroke"
+    , "#000C"
+    , "-strokewidth"
+    , "10"
+    , "-pointsize"
+    , ps
+    , "-annotate"
+    , "+0+10"
+    , text
+    , "-stroke"
+    , "none"
+    , "-fill"
+    , "white"
+    , "-pointsize"
+    , ps
+    , "-annotate"
+    , "+0+10"
+    , text
+  ]
+  where ps = show $ pointSize widthSize text
+
+pointSize :: Int -> [Char] -> Int
+pointsize _ "" = 0
+pointSize widthSize text
+  | widthSize <= 0  = 0
+  | otherwise       = truncate ((wsf * 0.4) / l * (72.0 / 34.0))
+  where wsf = fromIntegral widthSize
+        l   = fromIntegral (length text)
--- README.md
+++ README.md
@@ -18,13 +18,13 @@ Create animated GIFs, overlaid with optional text, from video files.
 ## CLI Usage
 
 ```bash
-gifcurry_cli ./in.mp4 ./out.gif start_second duration quality 'Optional top text.' 'Optional bottom text.'
+$ gifcurry_cli -i inputFile -o outputFile -s startTime -d durationTime -w widthSize -q qualityPercent -t topText -b bottomText
 ```
 
 ## CLI Example
 
 ```Bash
-~/gifcurry ❯❯❯ ./gifcurry_cli ./02_gran_dillama_1080p.mp4 ./out.gif 32 8 500 100 'What is' 'Gifcurry?'
+~/gifcurry ❯❯❯ ./gifcurry_cli -i ./02_gran_dillama_1080p.mp4 -o ./out.gif -s 32 -d 8 -w 500 -q 100 -t 'What is' -b 'Gifcurry?'
  _____ _  __                           
 |  __ (_)/ _|                          
 | |  \/_| |_ ___ _   _ _ __ _ __ _   _ 
@@ -37,6 +37,7 @@ gifcurry_cli ./in.mp4 ./out.gif start_second duration quality 'Optional top text
 Gifcurry (C) 2016 David Lettier. http://www.lettier.com/
 
 Input file: ./02_gran_dillama_1080p.mp4
+Output file: ./out.gif
 Start second: 32
 Duration: 8 seconds
 GIF width: 500px
@@ -56,6 +57,7 @@ Done.
   * [Graphics.UI.Gtk (gtk)](https://hackage.haskell.org/package/gtk3)
   * [System.Directory (directory)](https://hackage.haskell.org/package/directory)
   * [gtk2hs-buildtools](https://hackage.haskell.org/package/gtk2hs-buildtools)
+  * [cmdargs](https://hackage.haskell.org/package/cmdargs)
 * [FFmpeg](https://www.ffmpeg.org/download.html)
 * [ImageMagick](http://www.imagemagick.org/script/download.php)
 * [GTK+](http://www.gtk.org/download/index.php)
